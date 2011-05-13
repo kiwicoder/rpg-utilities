8a997eab1de2188abbfddd8af8d616d3ea7f6092
kiwicoder <kiwicoder@gmail.com>
2011-05-13 20:59:02 +0100

    Adding Maven Project

---
+++ rpg-utilities/src/main/java/com/arrggh/App.java
@@ -0,0 +1,13 @@
+package com.arrggh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
